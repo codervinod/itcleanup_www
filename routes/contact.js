9e788daa1ed23891e1a831206a679f6aead52c54
Vinod Gupta <vinod.gupta@nutanix.com>
2017-08-19 21:58:26 -0700

    Verify captch at server.

--- routes/contact.js
+++ routes/contact.js
@@ -1,14 +1,30 @@
 var express = require('express');
+var request = require('request');
+
 var router = express.Router();
 
+
 router.post('/form', function(req, res, next) {
-  console.log(req.body.name);
-  console.log(req.body.email);
-  console.log(req.body.subject);
-  console.log(req.body.message);
-  console.log(req.body["g_recaptcha_response"]);
-  res.status(200).json({ response: 'success' });
-  // res.status(200).json({ response: 'error', errorMessage:'problem' });
+  request.post('https://www.google.com/recaptcha/api/siteverify',
+      {form:{
+        secret:process.env.ITCLEANUP_RECAPTCHA_SECRET,
+        response:req.body["g_recaptcha_response"]
+        }
+      }, function callback(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var info = JSON.parse(body);
+            if (info.success) {
+                console.log(info.success);
+                console.log(req.body.name);
+                console.log(req.body.email);
+                console.log(req.body.subject);
+                console.log(req.body.message);
+                res.status(200).json({response: 'success'});
+                return;
+            }
+            res.status(200).json({response: 'error', errorMessage: 'problem'});
+        }
+      });
 });
 
 router.post('/refresh-captcha', function(req, res, next) {
