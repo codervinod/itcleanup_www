af78cd197f80a5bbe9b6b6b2869da332a92cbcbe
Vinod Gupta <vinod.gupta@nutanix.com>
2017-08-30 23:02:19 -0700

    Add newsletter function.

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/subscribe', function(req, res, next) {
-  res.status(200).json({response: 'success'});
-  // res.status(200).json({response: 'error', message: 'problem'});
+var Mailchimp = require('mailchimp-api-v3');
+var mailchimp = new Mailchimp(process.env.ITCLEANUP_MAILCHIMP_API_KEY);
+
+router.post('/subscribe', function (req, res, next) {
+    console.log(req.body.email);
+    mailchimp.post('/lists/4c06fe49f7/members', {
+        email_address: req.body.email,
+        status: 'subscribed'
+
+    })
+    .then(function (results) {
+        console.log(JSON.stringify(results));
+        res.status(200).json({response: 'success'});
+    })
+    .catch(function (err) {
+        console.log(err.message);
+        res.status(200).json({
+            response: 'error',
+            message: 'Already subscribed to newsletter'
+        });
+    });
 });
 
 module.exports = router;
